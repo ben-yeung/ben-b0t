1483bddc23fccc2c2b5a7cf4ca8c15b0d3cdee37
Ben Yeung <benjaminyeung@berkeley.edu>
2021-04-05 23:09:36 -0700

    Changed dir for command_temp.js

---
+++ command_temp.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js")
+const botconfig = require("../botconfig.json");
+const colours = require("../colours.json");
+
+module.exports = {
+    name: "",
+    description: "",
+    usage: "?",
+    execute(bot, message, args) {
+
+
+
+    }
+}
