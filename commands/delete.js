30ab0314fbfd050933bc90851e974589a435a6ce
Ben Yeung <benjaminyeung@berkeley.edu>
2021-07-02 23:16:02 -0700

    More specific feedback for delete comand

--- commands/delete.js
+++ commands/delete.js
@@ -5,22 +5,23 @@ const colours = require("../colours.json");
 module.exports = {
     name: "delete",
     description: "Moderator tool to delete slash commands",
-    usage: "?delete [slash command id] [optional: delete guild command? Leave blank if false]",
+    usage: "?delete [slash command id] [optional: delete global command? Leave blank if false]",
     async execute(bot, message, args) {
 
         if (!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply("You don't have that permission");
         if (!args[0]) return message.reply("No id given for slash command.")
         let id = args[0]
         try {
-            if (botconfig.GUILD_ID && args[1] == 'true') { //args[1] true if Guild Testing Command deletion
+            if (botconfig.GUILD_ID) { //args[1] true if Guild Testing Command deletion
                 console.log("In guild server")
                 await bot.api.applications(bot.user.id).guilds(botconfig.GUILD_ID).commands(id).delete()
-            } else {
+                return message.reply('Guild Slash command has been deleted')
+            } else if (args[1] == 'true') {
                 console.log("Not deleting guild command")
                 //await bot.api.applications(bot.user.id).commands(id).delete()
+                return message.reply('Global Slash command has been deleted')
             }
 
-            return message.reply('Slash command has been deleted')
         } catch (e) {
             console.log(e)
             return message.reply('Error occurred see console')
