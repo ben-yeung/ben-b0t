96cd340e785d39d5ccaecaed01966f047748b3a1
Ben Yeung <benjaminyeung@berkeley.edu>
2021-11-06 18:29:58 -0700

    Fixed Rotten Tomato embeds related to scores

--- commands/rottentomato.js
+++ commands/rottentomato.js
@@ -29,16 +29,53 @@ module.exports = {
             if (data.ok == true) {
                 let title = data.movie.name;
                 let score = `${data.movie.meterScore}`;
-                let scoreClass = data.movie.meterClass == 'certified_fresh' ? "Certified Fresh" : "Rotten";
+                var scoreClass = ""
+
+                switch (data.movie.meterClass) {
+                    case "fresh":
+                        scoreClass = "Fresh";
+                        break;
+                    case "certified_fresh":
+                        scoreClass = "Certified Fresh"
+                        break;
+                    case "rotten":
+                        scoreClass = "Rotten"
+                        break;
+                    default:
+                        scoreClass = "N/A"
+                        break;
+                }
+
                 let year = `${data.movie.year}`;
                 let url = data.movie.url;
                 let consensus = data.movie.consensus;
                 let actors = data.movie.actors.join(", ");
 
-                let fresh = 'https://www.pngitem.com/pimgs/m/138-1381056_rotten-tomatoes-fresh-logo-hd-png-download.png'
-                let flop = 'https://www.pngitem.com/pimgs/m/232-2328025_file-rotten-tomatoes-wikimedia-clipart-png-download-rotten.png'
-                let thumb = scoreClass == 'Certified Fresh' ? fresh : flop;
-                let color = scoreClass == 'Certified Fresh' ? colours.red_light : colours.green_dark;
+                let certfresh = 'https://www.rottentomatoes.com/static/images/icons/cf-lg.png'
+                let fresh = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Rotten_Tomatoes.svg/757px-Rotten_Tomatoes.svg.png'
+                let flop = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Rotten_Tomatoes_rotten.svg/1061px-Rotten_Tomatoes_rotten.svg.png'
+                let unknown = 'https://danielarussocoaching.nl/wp-content/uploads/2019/02/question-mark-1019820_1280.jpg'
+                var thumb;
+                var color;
+
+                switch (scoreClass) {
+                    case "Fresh":
+                        thumb = fresh;
+                        color = colours.red_light;
+                        break;
+                    case "Certified Fresh":
+                        thumb = certfresh;
+                        color = colours.red_light;
+                        break;
+                    case "Rotten":
+                        thumb = flop;
+                        color = colours.green_dark;
+                        break;
+                    default:
+                        thumb = unknown;
+                        color = colours.white;
+                        break;
+                }
 
                 let embed = new Discord.MessageEmbed()
                     .setTitle(`**${title}**`)
