4aa823384c5032f82c59d45ccac2ab8faa0dea57
Ben Yeung <benjaminyeung@berkeley.edu>
2021-12-02 10:45:04 -0800

    Fixed logic for modclear command

--- commands/modclear.js
+++ commands/modclear.js
@@ -8,28 +8,36 @@ const {
 module.exports = {
     name: "modclear",
     description: "Clear that clears all recent x messages",
-    usage: "?modclear [@user (optional)] [number]",
+    usage: "?modclear [optional: @user] [amount]",
     async execute(bot, message, args) {
 
         if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS)) return message.reply("Sorry, you don't have access to that command.");
 
         if (args.length == 2) {
-            let user = message.guild.member(message.mentions.users.first());
+            let user = message.mentions.users.first()
+            if (!user) return message.reply("No user found/mentioned. ?modclear [optional: @user] [amount]")
             const amount = parseInt(args[1]) + 1;
             if (isNaN(amount)) {
-                return message.reply("Command follows the format: ?modclear [user (optional)] [amount]")
+                return message.reply("Command follows the format: ?modclear [optional: @user] [amount]")
             }
             if (amount > 100) return message.reply('You can\'t delete more than 100 messages at once!'); // Checks if the `amount` integer is bigger than 100
             if (amount < 1) return message.reply('You have to delete at least 1 message!'); // Checks if the `amount` integer is smaller than 1
 
             try {
+                const toDelete = []
+                var count = 0;
                 await message.channel.messages.fetch({
                     limit: 100
-                }).then(messages => { // Fetches the messages
-                    const filterBy = user.user.id;
-                    messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
-                    message.channel.bulkDelete(messages);
-                });
+                }).then(async (messages) => {
+                    messages.filter((msg) => {
+                        if (count < amount && msg.author.id == user.id) {
+                            toDelete.push(msg);
+                            count += 1
+                        }
+                    })
+                })
+
+                message.channel.bulkDelete(toDelete, true);
             } catch (e) {
                 message.channel.send(`ERROR: ${e.message}`)
             }
@@ -44,12 +52,20 @@ module.exports = {
             if (amount < 1) return message.reply('You have to delete at least 1 message!'); // Checks if the `amount` integer is smaller than 1
 
             try {
+                const toDelete = []
+                var count = 0;
                 await message.channel.messages.fetch({
                     limit: 100
-                }).then(messages => { // Fetches the messages
-                    messages = messages.array().slice(0, amount);
-                    message.channel.bulkDelete(messages, true);
-                });
+                }).then(async (messages) => {
+                    messages.filter((msg) => {
+                        if (count < amount) {
+                            toDelete.push(msg);
+                            count += 1
+                        }
+                    })
+                })
+
+                message.channel.bulkDelete(toDelete, true);
 
             } catch (e) {
                 message.channel.send(`ERROR: ${e.message}`)
